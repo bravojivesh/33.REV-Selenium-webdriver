9ba33a6703a6ab058fb22151ef58b42cd2e0d25e
JIVESH HAMAL <bravojivesh@gmail.com>
2024-04-26 02:43:58 +0530

    need to click once enough money is earned

--- Cookies_clicker.py
+++ Cookies_clicker.py
@@ -3,6 +3,7 @@ from selenium.webdriver.common.by import By
 import time
 
 start_time=time.time()
+time_out=start_time+5
 
 #to leave the window open
 chrome_options=webdriver.ChromeOptions()
@@ -11,38 +12,45 @@ chrome_options.add_experimental_option("detach",True)
 driver=webdriver.Chrome(options=chrome_options)
 driver.get(url="https://orteil.dashnet.org/experiments/cookie/")
 
-while time.time()-start_time < 2: #total time
+while time.time()-start_time < 15: #total time
     cookie_to_click=driver.find_element(By.ID, value="cookie")
     cookie_to_click.click() #keep clicking
 
-time.sleep(0.5) #sometimes count is off by 1. So keeping this to make sure the count is correct.
-cookie_count=driver.find_element(By.ID, value="money")
+    #-------TO go through the items on the right panel------
+    #list_to_buy_object=driver.find_elements(By.ID, value="store")
+    #-------JH: the above returns ONE object only as ID is always unique. So we have to use by.CSS Selector
 
-#-------TO go through the items on the right panel------
+    if time_out-time.time() <5:
+        print (f"current time is:{time.time()} and timeout is: {time_out}")
+        time_out+=5
+        print (f"new time out is: {time_out}")
+        list_to_buy_object=driver.find_elements(By.CSS_SELECTOR, value="#store div") #returns a list of objects
 
-#list_to_buy_object=driver.find_elements(By.ID, value="store")
-#-------JH: the above returns ONE object only as ID is always unique. So we have to use by.CSS Selector
+        #To create a list based on the list of objects above, and access text attribute of of the object.
+        list_to_buy_human=[]
+        for item in list_to_buy_object:
+            list_to_buy_human.append(item.text)
 
-list_to_buy_object=driver.find_elements(By.CSS_SELECTOR, value="#store div") #returns a list of objects
+        # print(list_to_buy_human)
 
-#To create a list based on the list of objects above, and access text attribute of of the object.
-list_to_buy_human=[]
-for item in list_to_buy_object:
-    list_to_buy_human.append(item.text)
+        #to extract price from the long string
+        for item0 in list_to_buy_human:
+            if item0 !="": #NEED THIS as the last element is empty. So trying to split or replace below will give error
+                price= item0.split(" - ")[1].split("\n")[0]
+            #first split using "-". Then grab the item in [1] index. Split it again using "\n" and gram the first item.This only gives price.
+                price_without_commma=price.replace(",","") #for numbers like 2,000, they need to be converted into 2000
+                price_int=int(price_without_commma)
+                # print(f"the type so far is: {type(price_without_commma)}", price_without_commma)
+                # print (f"the type NOW is: {type(price_int)}",price_int)
+                cookie_count = driver.find_element(By.ID, value="money")
+                if int(cookie_count.text)> price_int:
+                    item0.cl
+                    print ("Can buy this:", item0)
 
-print(list_to_buy_human)
-
-#to extract price from the long string
-for item0 in list_to_buy_human:
-    if item0 !="": #NEED THIS as the last element is empty. So trying to split or replace below will give error
-        price= item0.split(" - ")[1].split("\n")[0]
-    #first split using "-". Then grab the item in [1] index. Split it again using "\n" and gram the first item.This only gives price.
-        price_without_commma=price.replace(",","")
-        price_int=int(price_without_commma)
-        print(f"the type so far is: {type(price_without_commma)}", price_without_commma)
-        print (f"the type NOW is: {type(price_int)}",price_int)
+            # if int(cookie_count) >
 
 
+# time.sleep(0.5) #sometimes count is off by 1. So keeping this to make sure the count is correct.
 
 # print ("Cookie score:", cookie_count.text)
 # print ("Start time was:", start_time)
